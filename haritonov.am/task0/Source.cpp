bad438cf557fae4f6449bd663d46666ea0d6701f
notirahk <alexeyxxl2001@gmail.com>
2019-09-16 22:17:49 +0300

    Комментарий 2

---
+++ haritonov.am/task0/Source.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+	double x1, y1, a, x2, y2, b, l, k, q;
+	printf("Введите a, (x1,y1) и b, (x2,y2)");
+	scanf_s("%lf %lf %lf %lf %lf %lf", &a, &x1, &y1, &b, &x2, &y2);
+	l = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+	if (b>a)  
+	{
+		k = b;
+		b = a;
+		a = k;
+	}
+	if (l > (a + b))
+		printf("1) Окружности не пересекаются");
+	else if (l == a + b)
+			printf("2) Окружности соприкасаются со внешней стороны");
+		 else if (l < a + b)
+				  if (l > a - b)
+					  printf("3) Окружности пересекаются");
+					  if ((l == a - b) && (l != 0))
+						  printf("4) Окружности соприкасаются изнутри");
+						  else if (l < a - b)
+						           printf("5) Одна окружность внутри другой");
+						           else if ((l = 0) && (a - b == 0))
+						                    printf("6) Окружности равны");
+scanf_s("%lf", &q);
+}
