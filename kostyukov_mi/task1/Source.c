4c3774b3debeb4b0e5a98c2f527391191899ccd6
maxkost <maximkostyukov@yandex.ru>
2019-09-18 16:04:08 +0300

    adjustment of task1

--- kostyukov_mi/task1/Source.c
+++ kostyukov_mi/task1/Source.c
@@ -2,29 +2,33 @@
 
 void main()
 {
-	const float DSV_P = 0.0008; // плотность ДСВ
-	const float DSP_P = 0.00085; // плотность ДСП
-	const float WOOD_P = 0.0008; // плотность дерева для дверей
+	const double DSP_P = 0.00085; // плотность ДСП
+	const double DVP_P = 0.0008; // плотность ДВП
+	const double WOOD_P = 0.0008; // плотность дерева для дверей
 
-	float w, h, d;
+	double h, w, d;
 
-	printf("w h d \n");
-	scanf_s("%f%f%f", &w, &h, &d);
+	printf("h w d \n");
+	scanf_s("%lf%lf%lf", &h, &w, &d);
 
-	float DVP_V = w * h * (0.5); //задняя стенка
-	
-	float DSP_V = 2 * h * d * (1.5); // две боковины
+	// Однострочное вычисление массы
+	// double m = 1.5 * DSP_P * d * (2 * (h + w) + (w - 3) * ((int)((h - 3) / 41.5))) + WOOD_P * h * w + DVP_P * 0.5 * h * w;
+	//	printf("%lf", m);
 
-	float WOOD_V = h * w; // двери
+	// Поэтапное вычисление массы
+	double DVP_V = h * w * 0.5; // объем задней стенки
 
-	DSP_V += 2 * w * d * (1.5); // верхняя и нижняя крышки
+	double DSP_V = 2 * h * d * 1.5; // объем боковин
 
-	int S = (h - 3) / (41.5); // кол-во полок
+	double WOOD_V = h * w; // объем дверей
 
-	DSP_V += S * d * w * (1.5); // полки
+	DSP_V += 2 * w * d * 1.5; // объем верхней и нижней крышек
 
-	float m = DSP_V * DSP_P + DVP_V * DSV_P + WOOD_V * WOOD_P; // масса шкафа
+	int S = (h - 3) / 41.5; // кол-во полок
 
-	printf("\n%f", m);
+	DSP_V += S * (w - 3) * d * (1.5); // объем полок
 
+	double m = DSP_V * DSP_P + DVP_V * DVP_P + WOOD_V * WOOD_P; // масса шкафа
+
+	printf("\n%lf", m);
 }
