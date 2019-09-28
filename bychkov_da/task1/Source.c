9b70f19ccc748fe91fa879a0d9bbc6342666e2ed
cjl31 <44344047+cjl31@users.noreply.github.com>
2019-09-28 23:12:56 +0300

    Uodate task1

--- bychkov_da/task1/Source.c
+++ bychkov_da/task1/Source.c
@@ -1,14 +1,18 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <math.h>
 #include <locale.h>
-int main(void) {
-	setlocale(LC_ALL, "Rus");
-	double h, w, d, pDVP, pDSP, pD, q, mZ, mB, mP, mD, M, k, x, y;
-	printf("Введите высоту(от 180 до 220 см), ширину(от 80 до 120 см), глубину(от 50 до 90 см) в сантиметрах ");
-	scanf_s("%lf %lf %lf", &h, &w, &d);
-	k = 0.5;
-	x = 1.5;
-	y = 1;
+int main(void) 
+{
+	char* locale = setlocale(LC_ALL, "");
+	float h, w, d, pDVP, pDSP, pD, q, mZ, mB, mP, mD, M, k = 0.5, x = 1.5, y = 1;
+	printf("Введите высоту боковой стенки(от 180 до 220 см), ширину задней стенки(от 80 до 120 см), глубину боковины(от 50 до 90 см) в сантиметрах ");
+	scanf_s("%f %f %f", &h, &w, &d);
+
+	if ((h < 180) || (h > 220) || (w < 80) || (w > 120) || (d < 50) || (d > 90))  
+	{
+		printf_s("Введены неверные данные");
+	}
+	else {
 	pDVP = 0.00085; //кг на кубический сантиметр
 	pDSP = 0.00065;
 	pD = 0.00045;
@@ -16,8 +20,9 @@ int main(void) {
 	mZ = k * h * w * pDVP; //масса задней стенки
 	mB = x * h * d * 2 * pDSP; //масса боковых стенок
 	mP = x * w * d * (2 + q) * pDSP; //масса полок и крышек
-	mD = y * w * h * 2 * pD; // масса дверей
+	mD = y * w * h * pD; // масса дверей
 	M = mZ + mB + mP + mD;
-	printf("Масса шкафа равна %lf кг.    ", M);
+	printf("Масса шкафа равна %f кг. ", M);
+	}
 	system("pause");
 }
