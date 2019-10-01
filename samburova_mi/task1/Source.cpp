b449b1de5e264405f2c9bd984afc58d7b6a54d35
samburovam <55128869+samburovam@users.noreply.github.com>
2019-10-01 09:39:17 +0300

    Исправленная версия

--- samburova_mi/task1/Source.cpp
+++ samburova_mi/task1/Source.cpp
@@ -15,18 +15,18 @@ void main()
 	printf("Введите ширину, высоту и глубину шкафа");
 	scanf_s("%lf, %lf, %lf" & h, &w, &d);
 	s = 0;
-	if ((h >= 0, 18) && (h <= 0, 22) && (w >= 0, 08) && (w <= 0, 12) && (d >= 0, 05) && (d <= 0, 09))
+	if ((h >= 0.18) && (h <= 0.22) && (w >= 0.08) && (w <= 0.12) && (d >= 0.05) && (d <= 0.09))
 	{
-		while (h >= 0, 08)
+		while (h >= 0.08)
 		{
-			h = h - 0, 0415;
+			h = h - 0.0415;
 			s = s + 1;
 		}
-		back = h * w * 0, 005 * DWP;
-		top_and_bot = d * w * 0, 015 * DSP * 2;
-		sidewalls = h * d * 0, 015 * DSP * 2;
-		shelfs = w * d * 0, 015 * s * DSP;
-		doors = h * w * 0, 01 * BLACKWOOD;
+		back = h * w * 0.005 * DWP;
+		top_and_bot = d * w * 0.015 * DSP * 2;
+		sidewalls = h * d * 0.015 * DSP * 2;
+		shelfs = w * d * 0.015 * s * DSP;
+		doors = h * w * 0.01 * BLACKWOOD;
 		m = back + top_and_bot + sidewalls + shelfs + doors;
 		printf("%lf, Масса шкафа-", m);
 
