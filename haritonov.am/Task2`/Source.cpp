c07f6f568cc0ed7ab7e63df00600ddaf84a8dec6
notirahk <alexeyxxl2001@gmail.com>
2019-10-03 20:41:25 +0300

    Task 2

---
+++ haritonov.am/Task2`/Source.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	char s;
+	int num, word, end;
+
+	word = num = 0;
+	printf("Введите строку\n");
+	while ((s=getchar()) != '\n')
+	{
+		if ((s >= '0') && (s <= '9'))
+		{
+			num = num + 1;
+			while (((s = getchar()) != ' ') && (s != '\n')) {}
+		}
+		else
+		{
+			word = word + 1;
+			while (((s = getchar()) != ' ') && (s != '\n')) {}
+		}
+		if (s == '\n')
+			break;
+	}
+	printf("Количество цифр = %i, Количество букв = %i\n", num, word);
+	scanf_s("%i", &end);
+}
