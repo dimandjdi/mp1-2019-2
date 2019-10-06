c07f87f00c5e5b94433ce69b6fac31ebaf3d70a1
AnasteishA <nastya.plenkova@mail.ru>
2019-10-06 16:10:12 +0300

    task2 and task2'

---
+++ plenkova_ad/task2'/task2'.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int a = 0, b = 0; //a -количество слов, b - количество чисел
+	char j;
+	printf("Введите строку символов\n");
+	do
+	{
+		j = getchar();
+		if ((j >= '0') && (j <= '9'))
+		{
+			b = b + 1;
+		}
+		else
+		{
+			a = a + 1;
+		}
+		while ((j != ' ') && (j != '\t') && (j != '\n'))
+		{
+			j = getchar();
+		}
+	} while (j != '\n');
+
+	printf("Количетсво слов = %d\nКоличество чисел = %d", a, b);
+}
