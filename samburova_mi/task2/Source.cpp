ed99a7325eff8f4b6442c2cddef6769aa7b4a6c4
samburovam <samburova.2001@mail.ru>
2019-10-17 18:33:05 +0300

    Исправленная версия, извините что так долго :с

--- samburova_mi/task2/Source.cpp
+++ samburova_mi/task2/Source.cpp
@@ -3,57 +3,60 @@
 #include <stdlib.h>
 int main()
 {
-	int n, k, i, b, p;
-	char ss[1];
+	int n, k, i, b, p, ff, max, min;
+	char ss;
 	printf("How do you want to play?\n");
 	printf("1.I want to guess by myself\n");
-	printf("2.Let the program works itself\n");
-	printf("Your choiñe:\n");
+	printf("2.Let the program work itself\n");
+	printf("Your choice:\n");
 	int input;
 	scanf_s("%d", &input);
 	switch (input) {
 	case 1:
+		i = 0;
 		srand(time(NULL));
 		n = rand() % 1001 + 1;
-		printf("Enter the number");
-		i = 0;
-		do 
-		{   
+		printf("Enter the number\n");
+		
+		do
+		{
 			scanf_s("%d", &k);
-			if (k = n) then
-				printf("You are right");
+			if (k == n)
+			printf("You are right\n");
 			else if (k < n)
-				printf("Number is upper");
-			else
-				printf("Number is lower");
-			i++;
-		} while (n != k);
-		printf("Òumber of attempts:%d", &i);
-		break;
+					printf("Number is upper\n");
+
+				else
+					printf("Number is lower\n");
+				i++;
+			} while (n != k);
+			printf("Number of attempts:%d\n", i);
+			break;
 	case 2:
-		 
-		do {
-			b = rand() % 1001 + 1;
-			printf("Is your number %d? If yes, enter '=', if your number is upper, enter '>', if number is lower, enter '<'\n", &b);
-			fgets(ss, 2, stdin);
-			fflush(stdin);
-			puts(ss);
-			if (ss = '=')
-			{
-				printf("Congratulations!");
-				break;
-			}
+		p = 0;
+		max = 1000;
+		min = 1;
+		do
+		{
+			
+			ff = (max + min) / 2;
+			printf("Is your number %d, upper or lower?\n", ff);
+			getchar();
+			scanf_s("%c", &ss);
+			if (ss == '>')
+				min = ff;
+			else if (ss == '<')
+				max = ff;
 			p++;
-		} while (ss = '=');
-		printf("Number of attempts:%d", &p);
+		} while (ss != '=');
+		printf ("Your number is %d\n", ff );
+		printf ("Amount of attempts:%d\n", p);
+
 		break;
 	default:
 		printf("Wrong choice");
-}
-		
-
-
-
-	}
+		}
+	system("pause>nul");
+	}	
 
 
---
+++ samburova_mi/task3/Source.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <locale.h>
+
+void main()
+int n;
+printf("Выберите длину загадываемого числа; от 2 до 5\n");
+scanf_s(%d, n);
+while (n < 2) || (n > 5) {
+	printf("Введите длину числа от 2 до 5");
+	scanf_s(% d, n);
+}
+	srand(time(NULL));
+
