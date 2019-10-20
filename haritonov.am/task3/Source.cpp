dcad3b6b15d604697ab1a5e29ea805f86852459b
notirahk <alexeyxxl2001@gmail.com>
2019-10-20 21:14:50 +0300

    Быки быкуют, а коровы поднимают бунт

--- haritonov.am/task3/Source.cpp
+++ haritonov.am/task3/Source.cpp
@@ -2,67 +2,92 @@
 #include <math.h>
 #include <locale.h>
 #include <time.h>
+#include <stdlib.h>
+															//Создание числа из n неповторяющихся цифр через массив
+int number(int n)
+{
+	int i = 1, k, j = 0, flag, num[5], x = 0;
+	num[4] = rand() % (9);
+	for (i; i != n; i++)
+	{
+		flag = 0;
+		while (!0)
+		{
+			k = rand() % (9);
+															//Проверка на повторяющиеся цифры через флаг
+			for (j; j != i; j++)
+				if (k == num[4-j])
+					flag = 1;
+			j = 0;
+			if ((flag == 0) && (i != n-1))
+			{
+				num[4 - i] = k;
+				break;
+			}
+															//Проверка на "0" 1-й цифрой
+			else if ((flag ==0) && (i == n-1) && (k != 0))
+			{
+				num[4 - i] = k;
+				break;
+			}
+			flag = 0;
+		}
+	}
+	i = 0;
+															//Превращение массива в число для вывода из функции
+	for (i; i != n; i++)
+		x = x + num[4-i] * pow(10, i);
+	return(x);
+}
+
 
 void main()
 {
 	srand(time(NULL));
 	setlocale(LC_ALL, "Russian");
+	int i, j, num, ask, ans[5], que[5], n, log, byk = 0, kor = 0, ch;
 
-	int x, y, num, q, t, i, j, z, m, n;
 
-	printf("Введите число от 2 до 5");
-	scanf_s("%int", &n);
-	switch (n)
+	printf("Введите число от 2 до 5\n");
+	scanf_s("%i", &n);
+	if ((n > 1) && (n < 6))
 	{
-	case 2:
-		y = 99; x = 10;
-		break;
-	case 3:
-		y = 999; x = 100;
-		break;
-	case 4:
-		y = 9999; x = 1000;
-		break;
-	case 5:
-		y = 99999; x = 10000;
-		break;
-	}
-	num = rand() % (y - x + 1) + x;
-	while (q != num)
-	{
-		scanf_s("%int", &q);
-		for (i = 0; i < n; ++i)
+		num = number(n);
+															//Превращение числа обратно в массив
+		for (i = 0; i != n; i++)
 		{
-			z = num % 10;
+			ans[4 - i] = num % 10;
 			num = num / 10;
-			for (j = 0; j < n; ++j)
-			{
-				m = q % 10;
-				q = q / 10;
-				if (z == m)
-					if ((z == m) && (i == j))
-						printf("Цифра %int угадана вплоть до позиции", m);
-					else printf("Цифра %int угадана, но не на верной позиции", m);
-			}
 		}
 	}
+	else exit(0);
+	printf("Угадывайте\n");
+	log = 1;
+	ch = 0;														//Сравнение чила пользователя и загаданного числа
+	while (log != 0)
+	{
+		scanf_s("%i", &ask);
+		for (i = 0; i != n; i++)
+		{
+			que[4 - i] = ask % 10;
+			ask = ask / 10;
+		}
+		for (i = 0; i != (n); i++)
+			for (j = 0; j != (n); j++)
+			{
+				if ((que[4 - i] == ans[4 - j]) && (i == j))
+					byk++;
+				else if ((que[4 - i] == ans[4 - j]) && (i != j))
+					kor++;
+			}
+		if (byk == n)
+			log = 0;
+		else printf("Быки - %i Коровы - %i\n", byk, kor);
+		byk = 0;
+		kor = 0;
+		ch++;
+	}
+	printf("Победа за %i\n попыток, молодец!", ch);
+	scanf_s("%i", &i);
+
 }
-
-
-/* там все дичь, смотри сюда
-	do
-	num = rand() % (y - x + 1) + x;
-	while(!((num % 10) != (num % 100)/10))
-	________
-	do
-	num = rand() % (y - x + 1) + x;
-	while(!((num % 10) != ((num % 100)/10) != ((num % 1000)/100)))
-	________
-	do
-	num = rand() % (y - x + 1) + x;
-	while(!((num % 10) != ((num % 100)/10) != ((num % 1000)/100) != ((num % 10000)/1000)))
-	________
-	do
-	num = rand() % (y - x + 1) + x;
-	while(!((num % 10) != ((num % 100)/10) != ((num % 1000)/100) != ((num % 10000)/1000) != ((num % 100000)/10000))) 
-*/
