14696d9b6a87cffa3d1fd5d5e9e2d6c05ff0e8d4
dariabnn <dariabnn@gmail.com>
2019-10-22 16:09:44 +0300

    исправлено задание 3

--- zaytseva_da/task3/Source.c
+++ zaytseva_da/task3/Source.c
@@ -48,16 +48,13 @@ int main()
 		}
 		b = 0;
 		k = 0;
-		for (i = 0; i < z; i++)
+		for ((i = 0; i < z; i++) && (j = 0; j < z; j++))
 		{
-			if (p[i] == m[i])
-				b++;
-			else
-				for (j = 0; j < z; j++)
-				{
-					if ((i != j) && (p[i] == m[j]))
-						k++;
-				}
+			if (p[i]==m[i])
+				if (i==j)
+					b++;
+				else
+					k++;
 		}
 		if (b == n)
 			break;
