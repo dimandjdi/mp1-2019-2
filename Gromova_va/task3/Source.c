09c32103246750c90e06491ae32456d94f4f489b
Громова Виктория <flapflapdany>
2019-10-22 21:31:41 +0300

    Исправленны ошибки в 3 задании

--- Gromova_va/task3/Source.c
+++ Gromova_va/task3/Source.c
@@ -13,8 +13,8 @@ int main()
  scanf_s(" %d", &n);
  while ((n > 5) || (n < 2))
  {
-	 printf("Введите число, соответствующее заданому диапазоную \n");
-	 scanf_s("%d", &n);
+   printf("Введите число, соответствующее заданому диапазоную \n");
+   scanf_s("%d", &n);
  }
   mass[0] = rand() % 9 + 1;
   for (i = 1; i < n; i++)
@@ -37,33 +37,33 @@ int main()
 	 scanf_s(" %d", &m);
 	 for (i = n - 1; i >= 0; i = i - 1)
 	 {
-			 mas[i] = m % 10;
-			 m /= 10;
+	   mas[i] = m % 10;
+	   m /= 10;
 	 }
-	 for ( i = 0; i < n; i++)
+	 for (i = 0; i < n; i++)
 	 {
-		 if (mass[i] == mas[i])
-		 {
-			 bulls++;
-		 }
+		if (mass[i] == mas[i])
+		{
+		  bulls++;
+	    }
 		 else
 		 {
 			 for (j = 0; j < n; j++)
 			 {
 				 if ((mass[i] == mas[j]) && (i != j))
 				 {
-					 cow++;
+				   cow++;
 				 }
-
 			 }
 		 }
-		 printf("Количество коров :%d \n", cow);
-		 printf("Количество быков: % d \n", bulls);
 	 }
+    printf("Количество коров :%d \n", cow);
+	printf("Количество быков: % d \n", bulls);
+	 
   } while (bulls != n);
   if (bulls = n)
   {
-		printf("Ты угадал число.)");
+	printf("Ты угадал число.)");
   }
   system("pause");
 }
