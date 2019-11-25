09f9841e4816297da7d8467db5ec9175fd061dc6
Ксения Стребкова <minigolov@gmail.com>
2019-11-25 23:30:27 +0300

    task5

--- strebkova_ko/task5/Source.c
+++ strebkova_ko/task5/Source.c
@@ -16,12 +16,12 @@ int countfile(char path[])
 	int count = 0;
 	if ((hFile = _findfirst(path, &c_file)) == -1L)
 		printf("В данной директории нет файлов\n");
-	else 
+	else
 		do {
 			count++;
 		} while (_findnext(hFile, &c_file) == 0);
-	_findclose(hFile);
-	return count;
+		_findclose(hFile);
+		return count;
 }
 
 int choice_sorting()
@@ -61,17 +61,17 @@ void SelectSort(struct _finddata_t *file, int n)
 {
 	int i, j, k;
 	struct _finddata_t x;
-	for (i = 0; i < n; i++) 
+	for (i = 0; i < n; i++)
 	{
-		k = i; 
+		k = i;
 		x = file[i];
 		for (j = i + 1; j < n; j++)
-			if (file[j].size < x.size) 
+			if (file[j].size < x.size)
 			{
-				k = j; 
+				k = j;
 				x = file[j];
 			}
-		file[k] = file[i]; 
+		file[k] = file[i];
 		file[i] = x;
 	}
 }
@@ -89,35 +89,40 @@ void InsertSort(struct _finddata_t *file, int n)
 	}
 }
 
-void merge(struct _finddata_t *file, long lb, long split, long ub) 
+void merge(struct _finddata_t *file, long lb, long split, long ub)
 {
-	long pos1 = lb, pos2 = split + 1, pos3 = 0;
+	long pos1 = lb;
+	long pos2 = split + 1;
+	long pos3 = 0;
 	struct _finddata_t *temp = (struct _finddata_t *)malloc((ub - lb + 1) * sizeof(struct _finddata_t));
-	while (pos1 <= split && pos2 <= ub) 
+	while (pos1 <= split && pos2 <= ub)
 	{
 		if (file[pos1].size < file[pos2].size)
 			temp[pos3++] = file[pos1++];
 		else
 			temp[pos3++] = file[pos2++];
 	}
+
 	while (pos2 <= ub)
 		temp[pos3++] = file[pos2++];
 	while (pos1 <= split)
 		temp[pos3++] = file[pos1++];
+
 	for (pos3 = 0; pos3 < ub - lb + 1; pos3++)
 		file[lb + pos3] = temp[pos3];
+
 	free(temp);
 }
 
-void MergeSort(struct _finddata_t *file, long lb, long ub) 
+void MergeSort(struct File *files, long lb, long ub)
 {
-	long split;                  
-	if (lb < ub) 
-	{            
+	long split;
+	if (lb < ub)
+	{
 		split = (lb + ub) / 2;
-		MergeSort(file, lb, split);      
-		MergeSort(file, split + 1, ub);
-		merge(file, lb, split, ub);    
+		MergeSort(files, lb, split);
+		MergeSort(files, split + 1, ub);
+		merge(files, lb, split, ub);
 	}
 }
 
@@ -128,16 +133,16 @@ void QuickSort(struct _finddata_t *file, int n)
 	long p;
 	p = file[n >> 1].size;
 	do {
-		while (file[i].size < p) 
+		while (file[i].size < p)
 			i++;
-		while (file[j].size > p) 
+		while (file[j].size > p)
 			j--;
-		if (i <= j) 
+		if (i <= j)
 		{
-			temp = file[i]; 
-			file[i] = file[j]; 
+			temp = file[i];
+			file[i] = file[j];
 			file[j] = temp;
-			i++; 
+			i++;
 			j--;
 		}
 	} while (i <= j);
@@ -145,15 +150,15 @@ void QuickSort(struct _finddata_t *file, int n)
 	if (n > i) QuickSort(file + i, n - i);
 }
 
-int increment(long inc[], long size) 
+int increment(long inc[], long size)
 {
 	int p1, p2, p3, s;
 	p1 = p2 = p3 = 1;
 	s = -1;
 	do {
-		if (++s % 2) 
+		if (++s % 2)
 			inc[s] = 8 * p1 - 6 * p2 + 1;
-		else 
+		else
 		{
 			inc[s] = 9 * p1 - 9 * p3 + 1;
 			p2 *= 2;
@@ -164,16 +169,16 @@ int increment(long inc[], long size)
 	return s > 0 ? --s : 0;
 }
 
-void ShellSort(struct _finddata_t *file, int n) 
+void ShellSort(struct _finddata_t *file, int n)
 {
 	long inc, i, j, seq[40];
 	int s;
 	struct _finddata_t temp;
 	s = increment(seq, n);
-	while (s >= 0) 
+	while (s >= 0)
 	{
 		inc = seq[s--];
-		for (i = inc; i < n; i++) 
+		for (i = inc; i < n; i++)
 		{
 			temp = file[i];
 			for (j = i - inc; (j >= 0) && (file[j].size > temp.size); j -= inc)
@@ -227,17 +232,17 @@ void outputfiles(struct _finddata_t *file, int n)
 	printf("FILE   %14c   SIZE\n", ' ');
 	switch (c)
 	{
-	    case 1:
-			for (i = 0; i < n; i++)
-				printf("%-12.12s   %.14c  %10ld\n", file[i].name, ' ', file[i].size);
-			break;
-		case 2:
-			for (i = n; i > 0; i--)
-				printf("%-12.12s %.14c  %10ld\n", file[i - 1].name, ' ', file[i - 1].size);
-			break;
+	case 1:
+		for (i = 0; i < n; i++)
+			printf("%-12.12s   %.14c  %10ld\n", file[i].name, ' ', file[i].size);
+		break;
+	case 2:
+		for (i = n; i > 0; i--)
+			printf("%-12.12s %.14c  %10ld\n", file[i - 1].name, ' ', file[i - 1].size);
+		break;
 	}
 }
- 
+
 void main()
 {
 	setlocale(LC_ALL, "Rus");
@@ -249,57 +254,57 @@ void main()
 	printf("Введите путь до директории:\n");
 	gets(path);
 	strcat(path, "*");
-	do
+	if ((n = countfile(path)) != 0)
 	{
-		if ((n = countfile(path)) != 0)
+		file = (struct _finddata_t*)malloc(n * sizeof(struct _finddata_t));
+		hFile = _findfirst(path, &file[0]);
+		for (i = 1; i < n; i++)
+			_findnext(hFile, &file[i]);
+		do
 		{
-			file = (struct _finddata_t*)malloc(n * sizeof(struct _finddata_t));
-			hFile = _findfirst(path, &file[0]);
-			for (i = 1; i < n; i++)
-				_findnext(hFile, &file[i]);
 			c = choice_sorting();
 			start = clock();
 			switch (c)
 			{
-			    case 1:
-				    BubbleSort(file, n);
-				    break;
-			    case 2:
-				    SelectSort(file, n);
-				    break;
-			    case 3:
-				    InsertSort(file, n);
-				    break;
-			    case 4:
-				    MergeSort(file, 0, n);
-				    break;
-			    case 5:
-				    QuickSort(file, n);
-				    break;
-			    case 6:
-				    ShellSort(file, n);
-				    break;
-			    case 7:
-				    CountingSort(file, n);
-				    break;
+			case 1:
+				BubbleSort(file, n);
+				break;
+			case 2:
+				SelectSort(file, n);
+				break;
+			case 3:
+				InsertSort(file, n);
+				break;
+			case 4:
+				MergeSort(file, 0, n - 1);
+				break;
+			case 5:
+				QuickSort(file, n);
+				break;
+			case 6:
+				ShellSort(file, n);
+				break;
+			case 7:
+				CountingSort(file, n);
+				break;
 			}
 			end = clock();
 			outputfiles(file, n);
 			printf("Время сортировки: %.30lf с\n", (double)(end - start) / CLOCKS_PER_SEC);
-			free(file);
-		}
-		printf("Введите 1 для новой сортировки, 2 - для изменения пути до директории, 0 - для завершения сортировки\n");
-		scanf_s("%d", &c);
-	} while (c == 1);
+			printf("Введите 1 для новой сортировки, 2 - для изменения пути до директории, 0 - для завершения сортировки\n");
+			scanf_s("%d", &c);
+		} while (c == 1);
+		free(file);
+	}
 	switch (c)
 	{
-	    case 0:
-		    exit(0);
-		    break;
-	    case 2:
-		    getchar();
-		    main();
-		    break;
+	case 0:
+		exit(0);
+		break;
+	case 2:
+		getchar();
+		main();
+		break;
 	}
 	system("pause");
 }
