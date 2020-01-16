a831379dee0d4a0fd38f57c07e90888f2b89b525
AnasteishA <nastya.plenkova@mail.ru>
2020-01-16 21:44:02 +0300

    и снова task6))))

--- plenkova_ad/task6/Source.c
+++ plenkova_ad/task6/Source.c
@@ -5,78 +5,91 @@
 #include <stdlib.h>
 #include <conio.h>
 
+double exp_etalon(double x)
+{
+	double etalon = exp(x);
+	return etalon;
+}
+double sinus_etalon(double x)
+{
+	x *= M_PI / 180;
+	double etalon = sin(x);
+	return etalon;
+}
+double cosinus_etalon(double x)
+{
+	x *= M_PI / 180;
+	double etalon = cos(x);
+	return etalon;
+}
+double shinus_etalon(double x)
+{
+	x *= M_PI / 180;
+	double etalon = sinh(x);
+	return etalon;
+}
+double coshinus_etalon(double x)
+{
+	x *= M_PI / 180;
+	double etalon = cosh(x);
+	return etalon;
+}
 
-void exponenta(double x, int N, double tochnost)
+double exponenta(double x, int N, double tochnost)
 {
 	int i;
 	double znachenie = 1, znach1 = x;
-	double etalon = exp(x);
-	for (i = 1; i < N; i++)
+	for (i = 1; i <= N; i++)
 	{
 		znachenie += znach1;
 		znach1 *= x / (i + 1);
-		if (fabs(znachenie - etalon) < tochnost)
+		if (fabs(znachenie - exp(x)) < tochnost)
 			break;
 	}
 	N = i - 1;
-	double raznitsa = fabsl(znachenie - etalon);
-	printf("Эталонное значение - %lf\n", etalon);
-	printf("Полученное значение - %lf\n", znachenie);
-	printf("Разница - %lf\n", raznitsa);
-	printf("Количество слагаемых - %d\n", N);
-
+	return znachenie;
 }
-void sinus(double x, int N, double tochnost)
+double sinus(double x, int N, double tochnost)
 {
 	int i;
 	x *= M_PI / 180;
 	double znachenie = 0, znach1 = 1;
-	double etalon = sin(x);
 	znach1 = x;
-	for (i = 1; i < N; i++)
+	for (i = 1; i <= N; i++)
 	{
 		znachenie += znach1;
 		znach1 *= ((-1) * x * x) / ((2 * i) * (2 * i + 1));
-		if (fabs(znachenie - etalon) < tochnost)
+		if (fabs(znachenie - sin(x)) < tochnost)
 			break;
 	}
 	N = i - 1;
-	double raznitsa = fabsl(znachenie - etalon);
-	printf("Эталонное значение - %lf\n", etalon);
-	printf("Полученное значение - %lf\n", znachenie);
-	printf("Разница - %lf\n", raznitsa);
-	printf("Количество слагаемых - %d\n", N);
+	double raznitsa = fabsl(znachenie - sin(x));
+	return znachenie;
 
 }
-void cosinus(double x, int N, double tochnost)
+double cosinus(double x, int N, double tochnost)
 {
 	x *= M_PI / 180;
 	int i;
 	double znachenie = 0, znach1 = 1;
-	double etalon = cos(x);
-	for (i = 1; i < N; i++)
+
+	for (i = 1; i <= N; i++)
 	{
 		znachenie += znach1;
 		znach1 *= ((-1) * x * x) / ((2 * i) * (2 * i - 1));
-		if (fabs(znachenie - etalon) < tochnost)
+		if (fabs(znachenie - cos(x)) < tochnost)
 			break;
 	}
 	N = i - 1;
-	double raznitsa = fabsl(znachenie - etalon);
-	printf("Эталонное значение - %lf\n", etalon);
-	printf("Полученное значение - %lf\n", znachenie);
-	printf("Разница - %lf\n", raznitsa);
-	printf("Количество слагаемых - %d\n", N);
-
+	double raznitsa = fabsl(znachenie - cos(x));
+	return znachenie;
 }
-void koshinus(double x, int N, double tochnost)
+double coshinus(double x, int N, double tochnost)
 {
 	x *= M_PI / 180;
 	int i;
 	double znach1 = 1;
 	double znachenie = 1;
-	double etalon = cosh(x);
-
 	for (i = 2; i <= N; i++)
 	{
 		znach1 *= (x / (i - 1));
@@ -86,131 +99,50 @@ void koshinus(double x, int N, double tochnost)
 			znachenie += znach1;
 		}
 
-		if (fabs(znachenie - etalon) < tochnost)
+		if (fabs(znachenie - cosh(x)) < tochnost)
 			break;
 	}
 	N = i - 1;
-	double raznitsa = fabsl(znachenie - etalon);
-	printf("Эталонное значение - %lf\n", etalon);
-	printf("Полученное значение - %lf\n", znachenie);
-	printf("Разница - %lf\n", raznitsa);
-	printf("Количество слагаемых - %d\n", N);
+	double raznitsa = fabsl(znachenie - cosh(x));
+	return znachenie;
 }
-void shinus(double x, int N, double tochnost)
+double shinus(double x, int N, double tochnost)
 {
 	x *= M_PI / 180;
 	int i;
 	double znach1 = x;
 	double znachenie = x;
-	double etalon = sinh(x);
+
 	for (i = 2; i <= N; i++)
 	{
 		znach1 *= ((x * x) / ((2 * i - 2) * (2 * i - 1)));
 		znachenie += znach1;
 	}
 	N = i - 1;
-	double raznitsa = fabsl(znachenie - etalon);
-	printf("Эталонное значение - %lf\n", etalon);
-	printf("Полученное значение - %lf\n", znachenie);
-	printf("Разница - %lf\n", raznitsa);
-	printf("Количество слагаемых - %d\n", N);
+	double raznitsa = fabsl(znachenie - sinh(x));
+	return znachenie;
 }
-
-
-void Exponenta(double x, int N)
-{
-	int i;
-	double znachenie = 1, znach1 = x;
-	double etalon = exp(x);
-	for (i = 1; i <= N; i++)
-	{
-		znachenie += znach1;
-		znach1 *= x / (i + 1);
-		printf("%2d %20lf %25lf %20lf\n", i, etalon, znachenie, fabs(etalon - znachenie));
-	}
-
-}
-void Sinus(double x, int N)
-{
-	int i;
-	x *= M_PI / 180;
-	double znachenie = 0, znach1 = 1;
-	double etalon = sin(x);
-	znach1 = x;
-	for (i = 1; i <= N; i++)
-	{
-		znachenie += znach1;
-		znach1 *= ((-1) * x * x) / ((2 * i) * (2 * i + 1));
-		printf("%2d %20lf %25lf %20lf\n", i, etalon, znachenie, fabs(etalon - znachenie));
-	}
-}
-void Cosinus(double x, int N)
-{
-	x *= M_PI / 180;
-	int i;
-	double znachenie = 0, znach1 = 1;
-	double etalon = cos(x);
-	for (i = 1; i <= N; i++)
-	{
-		znachenie += znach1;
-		znach1 *= ((-1) * x * x) / ((2 * i) * (2 * i - 1));
-		printf("%2d %20lf %25lf %20lf\n", i, etalon, znachenie, fabs(etalon - znachenie));
-	}
-
-}
-void Koshinus(double x, int N)
-{
-	x *= M_PI / 180;
-	int i;
-	double znach1 = 1;
-	double znachenie = 1;
-	double etalon = cosh(x);
-
-	for (i = 2; i <= N; i++)
-	{
-		znach1 *= (x / (i - 1));
-
-		if (i % 2 == 1)
-		{
-			znachenie += znach1;
-		}
-		printf("%2d %20lf %25lf %20lf\n", i, etalon, znachenie, fabs(etalon - znachenie));
-	}
-}
-void Shinus(double x, int N)
-{
-	x *= M_PI / 180;
-	int i;
-	double znachenie = x, znach1 = x;
-	double etalon = sinh(x);
-	for (i = 2; i <= N; i++)
-	{
-		znach1 *= ((x * x) / ((2 * i - 2) * (2 * i - 1)));
-		znachenie += znach1;
-		printf("%2d %20lf %25lf %20lf\n", i, etalon, znachenie, fabs(etalon - znachenie));
-	}
-}
-
 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	int vibor_fuctii = 0, Chislo_slagaemih = 0, NMax = 0, mode = 0;
-	double tochnost, tochka_x, raznitsa = 0, znachenie = 0;
+	int vibor_fuctii, Chislo_slagaemih, NMax = 0, mode;
+	double tochnost, tochka_x, raznitsa, znachenie;
 
-	void (*function1[5]) (double, int, double);
+	double (*function1[5]) (double, int, double);
 	function1[0] = sinus;
 	function1[1] = cosinus;
 	function1[2] = exponenta;
-	function1[3] = koshinus;
+	function1[3] = coshinus;
 	function1[4] = shinus;
 
-	void(*function_series[5])(double, int);
-	function_series[0] = Sinus;
-	function_series[1] = Cosinus;
-	function_series[2] = Exponenta;
-	function_series[3] = Koshinus;
-	function_series[4] = Shinus;
+	double(*Etalonnye_znacheniya[5])(double);
+	Etalonnye_znacheniya[0] = sinus_etalon;
+	Etalonnye_znacheniya[1] = cosinus_etalon;
+	Etalonnye_znacheniya[2] = exp_etalon;
+	Etalonnye_znacheniya[3] = shinus_etalon;
+	Etalonnye_znacheniya[4] = coshinus_etalon;
+
 	printf("~~~~~~~~~~~~~~~~~~\n");
 	printf("Хеллоу!\nВыберите режим:\n1 - Посчитать АДЫН РАЗ,\n2 - Провести несколько рассчётов\n");
 	scanf_s("%d", &mode);
@@ -234,7 +166,10 @@ int main()
 		printf("Сколько элементов должно быть в ряду? (от 1 до 1000):\n");
 		scanf_s("%d", &Chislo_slagaemih);
 
-		function1[vibor_fuctii - 1](tochka_x, Chislo_slagaemih, tochnost);
+		printf("Эталонное значение - %lf\n", Etalonnye_znacheniya[vibor_fuctii - 1](tochka_x));
+		printf("Полученное значение - %lf\n", function1[vibor_fuctii - 1](tochka_x, Chislo_slagaemih, tochnost));
+		printf("Разница в значениях - %lf\n", fabsl(Etalonnye_znacheniya[vibor_fuctii - 1](tochka_x) - function1[vibor_fuctii - 1](tochka_x, Chislo_slagaemih, tochnost)));
+		printf("Число слагаемых - %d\n", Chislo_slagaemih);
 		break;
 	case 2:
 		while ((NMax < 1) || (NMax > 25))
@@ -242,9 +177,15 @@ int main()
 			printf("Сколько провести экспериментов? (от 1 до 25)\n");
 			scanf_s("%d", &NMax);
 		}
+		int j = 1;
 		printf(" №         ЧТО ДОЛЖНО БЫТЬ          ЧТО ПОЛУЧИЛОСЬ           РАЗНИЦА\n");
 		printf(" ~              ~~~~~~                   ~~~~~~~              ~~~~~~\n");
-		function_series[vibor_fuctii - 1](tochka_x, NMax);
+		for (int i = 0; i < NMax; i++)
+		{
+			printf("%2d %20lf %24lf %20lf\n", j++, Etalonnye_znacheniya[vibor_fuctii - 1](tochka_x), function1[vibor_fuctii - 1](tochka_x, i + 1, tochnost), fabsl(Etalonnye_znacheniya[vibor_fuctii - 1](tochka_x) - function1[vibor_fuctii - 1](tochka_x, i + 1, tochnost)));
+
+		}
+		break;
 	}
 	system("pause");
 	return 0;
